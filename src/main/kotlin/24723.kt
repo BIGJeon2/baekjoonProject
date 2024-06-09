1a48b10ebd8092d6adf9640ffa502d0628ef5360
BIGJeon2 <robert1157311@gmail.com>
2024-06-09 23:11:16 +0900

    24723 - pow

--- src/main/kotlin/24723.kt
+++ src/main/kotlin/24723.kt
@@ -2,7 +2,7 @@ package org.example
 
 import kotlin.math.pow
 
-fun main() = with(System.`in`.bufferedReader()) {
+fun BaekJoon_24723() = with(System.`in`.bufferedReader()) {
     val sb = StringBuilder()
     sb.append((2.0).pow(Integer.parseInt(readLine())).toInt())
     print(sb)
