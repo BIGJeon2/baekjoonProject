5bb24edadd4b9b122e43ceffd64aa350869128ba
BIGJeon2 <robert1157311@gmail.com>
2024-06-24 10:04:01 +0900

    1300 - K 번째 수 (이분탐색 X)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,20 @@
 package org.example
 
-import kotlin.math.pow
-
-val sb = StringBuilder()
+import kotlin.math.max
 
 fun main() = with(System.`in`.bufferedReader())  {
-    val input = readLine().toInt()
-    for(i in 0 until input){
-        for (j in 0 until input){
-            star(input, i, j)
+    val arraySize = readLine().toInt()
+    val outPutIndex = readLine().toInt()
+    val numList = mutableListOf<Int>()
+
+    for (i in 1 .. arraySize){
+        for (j in 1 .. arraySize){
+            numList.add(i*j)
         }
-        sb.append("\n")
     }
 
-    print(sb)
-}
-fun star(total: Int, col: Int, row: Int){
-    if ((col / total) % 3 == 1 && (row / total) % 3 == 1){
-        sb.append(" ")
-    }else if (total / 3 == 0){
-        sb.append("*")
-    }else{
-        star(total/3, row, col)
-    }
+    numList.sort()
+
+    println(numList[outPutIndex-1])
+
 }
