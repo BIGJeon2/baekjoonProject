8d767a4c395efa46c1d28c83ddbc27ae0770bc31
BIGJeon2 <robert1157311@gmail.com>
2024-07-08 10:24:58 +0900

    1520(Not yet)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,16 +4,37 @@ import kotlin.Long.Companion.MAX_VALUE
 import kotlin.math.min
 
 fun main() = with(System.`in`.bufferedReader()) {
-    val n = readLine().toInt()
-    val inputArr = Array(n+1){(LongArray(2))}
-    val dp = Array(n+1) {LongArray(n+1)}
+    val (n,m) = readLine().split(" ").map { it.toInt() }
+    val inputArr = Array(n){LongArray(m)}
+    var result = n * (m-1)
+    var count = 0
 
-    for (i in 1 ..n){
+    for (i in 0 until n){
         val longArr = readLine().split(" ").map { it.toLong() }.toLongArray()
         inputArr[i]= longArr
     }
 
-    for (i in 2..n) {
+    for (i in 0 until n){
+        for (j in 0 until m){
+            val value = inputArr[i][j]
+            if (i != n - 1 && j != m - 1){
+                if (value >= inputArr[i][j+1] || value >= inputArr[i+1][j]){
+                    result--
+                }
+            }else if (i == n - 1 && j != m - 1){
+                if (value >= inputArr[i][j+1]){
+                    result--
+                }
+            }else if (i != n - 1 && j == m - 1){
+                if (value >= inputArr[i+1][j]){
+                    result--
+                }
+            }
+        }
+    }
+    print(result)
+    print(count)
+    /*for (i in 2..n) {
         for (j in i-1 downTo 1) {
             dp[j][i] = MAX_VALUE
             for (k in j until  i) {
@@ -24,5 +45,5 @@ fun main() = with(System.`in`.bufferedReader()) {
         }
     }
 
-    print(dp[1][n])
+    */
 }
