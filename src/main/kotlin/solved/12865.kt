5324d00bfb4fad05c4a5a5f69597dedc6d2318ad
BIGJeon2 <robert1157311@gmail.com>
2024-07-29 20:09:57 +0900

    12865 - DP

--- src/main/kotlin/solved/12865.kt
+++ src/main/kotlin/solved/12865.kt
@@ -1,18 +1,18 @@
-package org.example
+package org.example.solved
 
 import kotlin.math.max
 
-fun main() = with(System.`in`.bufferedReader()) {
+fun BaekJoon_12865() = with(System.`in`.bufferedReader()) {
     val (n, m) = readLine().toString().split(" ").map { it.toInt() }
     val weightArr = IntArray(n + 1)
     val qualityArr = IntArray(n + 1)
     val dp = Array(n + 1) { Array(m + 1) { 0 } }
 
-        repeat(n) {
-            val (weight, quality) = readLine().toString().split(" ").map { it.toInt() }
-            weightArr[it + 1] = weight
-            qualityArr[it + 1] = quality
-        }
+    repeat(n) {
+        val (weight, quality) = readLine().toString().split(" ").map { it.toInt() }
+        weightArr[it + 1] = weight
+        qualityArr[it + 1] = quality
+    }
 
     for (i in 1..n) {
         for (j in 1 .. m) {
