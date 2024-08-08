d3bab0a07d84aa923ec5cfc376cfb6a1cce3f0dd
BIGJeon2 <robert1157311@gmail.com>
2024-08-08 14:46:27 +0900

    11444 - DP(피보나치 행열 곱셈 전환)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,14 @@
 package org.example
 
 fun main() = with(System.`in`.bufferedReader()) {
-    val (n, m) = readLine().toString().split(" ").map { it.toLong() }
-    val matrix = Array(n.toInt()) { readLine().split(' ').map { it.toLong() % 1000L }.toLongArray() }
-
-    for (i in pow(matrix, m)){
-        for (j in i) {
-            print("${j} ")
-        }
-        println()
-    }
+    val n = readLine().toLong()
+    val matrix = arrayOf(longArrayOf(1L, 1L), longArrayOf(1L, 0L))
+    print("${pow(matrix, n-1)[0][0]}")
 }
 
 fun pow(matrix: Array<LongArray>, exponent: Long): Array<LongArray>{
     return when(exponent){
-        1L -> matrix
+        0L, 1L -> matrix
         else -> {
             val n = pow(matrix, exponent / 2)
             if (exponent % 2 == 0L) multi(n, n)
@@ -24,14 +18,13 @@ fun pow(matrix: Array<LongArray>, exponent: Long): Array<LongArray>{
 }
 
 fun multi(mat1: Array<LongArray>, mat2: Array<LongArray>): Array<LongArray> {
-    val size = mat1.size
-    val tmp = Array(size) { LongArray(size) }
+    val tmp = Array(2) { LongArray(2) }
 
-    for (i in 0 until size) {
-        for (j in 0 until size) {
-            for (k in 0 until size) {
+    for (i in 0 until 2) {
+        for (j in 0 until 2) {
+            for (k in 0 until 2) {
                 tmp[i][j] += mat1[i][k] * mat2[k][j]
-                tmp[i][j] %= 1000L
+                tmp[i][j] %= 1000000007L
             }
         }
     }
---
+++ src/main/kotlin/solved/11444.kt
@@ -0,0 +1,33 @@
+package org.example.solved
+
+fun BaekJoon_11444() = with(System.`in`.bufferedReader()) {
+    val n = readLine().toLong()
+    val matrix = arrayOf(longArrayOf(1L, 1L), longArrayOf(1L, 0L))
+    print("${pow(matrix, n-1)[0][0]}")
+    fun pow(matrix: Array<LongArray>, exponent: Long): Array<LongArray>{
+        return when(exponent){
+            0L, 1L -> matrix
+            else -> {
+                val n = pow(matrix, exponent / 2)
+                if (exponent % 2 == 0L) multi(n, n)
+                else multi(multi(n, n), matrix)
+            }
+        }
+    }
+
+    fun multi(mat1: Array<LongArray>, mat2: Array<LongArray>): Array<LongArray> {
+        val tmp = Array(2) { LongArray(2) }
+
+        for (i in 0 until 2) {
+            for (j in 0 until 2) {
+                for (k in 0 until 2) {
+                    tmp[i][j] += mat1[i][k] * mat2[k][j]
+                    tmp[i][j] %= 1000000007L
+                }
+            }
+        }
+
+        return tmp
+    }
+}
+
