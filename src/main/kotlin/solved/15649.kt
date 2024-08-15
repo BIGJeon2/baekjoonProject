b5625423c911241b1a4567fed291e8a321354d84
BIGJeon2 <robert1157311@gmail.com>
2024-08-15 13:35:48 +0900

    15649 - DFS

---
+++ src/main/kotlin/solved/15649.kt
@@ -0,0 +1,25 @@
+package org.example.solved
+
+fun BaekJoon_15649() = with(System.`in`.bufferedReader()) {
+    val (n, m) = readLine().split(" ").map { it.toInt() }
+    val numList = Array(n + 1){ i -> i}
+    val visitedList = Array(n + 1){ false}
+
+    fun dfs(index: Int, length: Int, answer: String){
+        if (length == m){
+            println(answer)
+            return
+        }
+
+        for (i in 1 .. n){
+            if (!visitedList[i]){
+                visitedList[i] = true
+                if (length == 0) dfs(i, 1, numList[i].toString())
+                else dfs(i, length + 1, answer + " " + numList[i].toString())
+                visitedList[i] = false
+            }
+        }
+    }
+
+    dfs(1, 0, "")
+}
