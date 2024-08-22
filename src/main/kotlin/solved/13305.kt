9a00b2528f1c2d045c58e4981aa5219fc3a84fe5
BIGJeon2 <robert1157311@gmail.com>
2024-08-22 12:32:58 +0900

    13305

--- src/main/kotlin/solved/13305.kt
+++ src/main/kotlin/solved/13305.kt
@@ -1,19 +1,16 @@
 package org.example.solved
 
+import java.util.*
+
 fun BaekJoon_13305() = with(System.`in`.bufferedReader()) {
-    readLine().toInt()
-    val roadList = readLine().split(" ").map { it.toInt() }
-    val priceList = readLine().split(" ").map { it.toInt() }
-    var currPrice = priceList[0]
-    var neededPrice = 0
-
-    for (i in roadList.indices){
-        val nextPrice = priceList[i]
-        neededPrice += currPrice * roadList[i]
-        if (nextPrice < currPrice) {
-            currPrice = nextPrice
-        }
+    val n = readLine().toInt() - 1
+    val distance = StringTokenizer(readLine()).run { LongArray(n) { nextToken().toLong() } }
+    val price = StringTokenizer(readLine()).run { LongArray(n) { nextToken().toLong() } }
+    var minPrice = price[0]
+    var total = 0L
+    for (i in 0 until n) {
+        minPrice = minOf(minPrice, price[i])
+        total += minPrice * distance[i]
     }
-
-    print(neededPrice)
+    println(total)
 }
